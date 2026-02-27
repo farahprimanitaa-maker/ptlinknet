a6f14aa06b1f5ee3945ec7547c041beb3232a841
farahprimanitaa-maker <farahprimanitaa@gmail.com>
2026-02-27 13:40:34 +0700

    farah.sql

--- farah.sql
+++ farah.sql
@@ -1,4 +1,4 @@
----section 1---
+---section 1.1---
 WITH itungan AS (
     SELECT
         a.region,
@@ -14,6 +14,48 @@ WITH itungan AS (
 
 SELECT
     region,
+    activesubscount,
+    homepasscount,
+    activesubscount * 1.0 / NULLIF(homepasscount, 0) AS penetration_rate
+FROM itungan;
+
+---section 1.2---
+WITH itungan AS (
+    SELECT
+        a.technology,
+        COUNT(DISTINCT CASE 
+            WHEN b.status = 'ACTIVE' THEN b.homeid 
+        END) AS activesubscount,
+        COUNT(DISTINCT a.homeid) AS homepasscount
+    FROM homepass_fin a
+    LEFT JOIN subscription_snapshot_fin b
+        ON a.homeid = b.homeid
+    GROUP BY a.technology
+)
+
+SELECT
+    technology,
+    activesubscount,
+    homepasscount,
+    activesubscount * 1.0 / NULLIF(homepasscount, 0) AS penetration_rate
+FROM itungan;
+
+---section 1.3---
+WITH itungan AS (
+    SELECT
+        a.exclusive_flag,
+        COUNT(DISTINCT CASE 
+            WHEN b.status = 'ACTIVE' THEN b.homeid 
+        END) AS activesubscount,
+        COUNT(DISTINCT a.homeid) AS homepasscount
+    FROM homepass_fin a
+    LEFT JOIN subscription_snapshot_fin b
+        ON a.homeid = b.homeid
+    GROUP BY a.technology
+)
+
+SELECT
+    exclusive_flag,
     activesubscount,
     homepasscount,
     activesubscount * 1.0 / NULLIF(homepasscount, 0) AS penetration_rate
